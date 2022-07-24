6b6d5f9f75163d5c6d5a51d89171ee51745b0e2b
Michal Wisniewski <michu123qwe@gmail.com>
2022-07-24 15:02:49 +0200

    Add navigation drawer (wip).

--- app/src/main/java/pl/mwisniewski/workoutapp/MainActivity.kt
+++ app/src/main/java/pl/mwisniewski/workoutapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package pl.mwisniewski.workoutapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        // TODO: she drawer shows up but doesnt open
+        findViewById<NavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
