5df2b94c472cedb88a9dd7fc2c3c8b6fbf1cd00e
Michal Wisniewski <michu123qwe@gmail.com>
2022-07-25 16:46:19 +0200

    Fix navigation drawer.

--- app/src/main/java/pl/mwisniewski/workoutapp/MainActivity.kt
+++ app/src/main/java/pl/mwisniewski/workoutapp/MainActivity.kt
@@ -3,13 +3,17 @@ package pl.mwisniewski.workoutapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,11 +23,15 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
-        // TODO: she drawer shows up but doesnt open
+        setupActionBarWithNavController(navController, appBarConfiguration)
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
