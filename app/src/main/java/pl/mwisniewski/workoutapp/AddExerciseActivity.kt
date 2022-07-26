bf6aca54e773ce16e8a8ba6bc72492ac886892a2
Michal Wisniewski <michu123qwe@gmail.com>
2022-07-26 17:55:43 +0200

    Add ui.

--- app/src/main/java/pl/mwisniewski/workoutapp/AddExerciseActivity.kt
+++ app/src/main/java/pl/mwisniewski/workoutapp/AddExerciseActivity.kt
@@ -1,11 +1,22 @@
 package pl.mwisniewski.workoutapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import pl.mwisniewski.workoutapp.domain.model.Category
 
 class AddExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_exercise)
+
+        val adapter = ArrayAdapter(
+            this, android.R.layout.simple_spinner_item, Category.values()
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val spinner = findViewById<Spinner>(R.id.add_exercise_category_spinner)
+        spinner.run { this.adapter = adapter }
     }
 }
